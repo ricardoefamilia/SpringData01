78a89bb2406c56ce28c08259cf83742ecd24429a
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-20 15:25:01 -0300

    InterfaceSpringDataUser - 20/01/22 - 15h25.

---
+++ SpringData01/src/main/java/com/SpringData01/dao/InterfaceSpringDataUser.java
@@ -0,0 +1,11 @@
+package com.SpringData01.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SpringData01.model.Usuario;
+
+@Repository
+public interface InterfaceSpringDataUser extends CrudRepository<Usuario, Long>{	
+	
+}
--- SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
+++ SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
@@ -5,10 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.SpringData01.dao.InterfaceSpringDataUser;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppSpringDataTest {
 
+	//interface para realizar os métodos de CRUD do spring.
+	private InterfaceSpringDataUser interfaceSpringDataUser;
+	
 	@Test
 	public void testeInsert() {
 		System.out.println("Iniciou o Spring com sucesso!!!");
