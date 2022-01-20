4474f5af74c9c269244a9d6b9f251b204148ac0e
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-20 15:41:50 -0300

    InterfaceSpringDataUser - 20/01/22 - 15h40.

--- SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
+++ SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.SpringData01.dao.InterfaceSpringDataUser;
+import com.SpringData01.model.Usuario;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
@@ -16,7 +17,18 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testeInsert() {
-		System.out.println("Iniciou o Spring com sucesso!!!");
+		//System.out.println("Iniciou o Spring com sucesso!!!");
+		Usuario usuario = new Usuario();
+		usuario.setEmail("ricardo@gmail.com");
+		usuario.setIdade(51);
+		usuario.setId(1L);
+		usuario.setLogin("ricardo");
+		usuario.setNome("Ricardo Lima Amaral");
+		usuario.setSenha("r1234");
+		
+		//interfaceSpringDataUser.save(usuario);
+		
+		//System.out.println("Dados gravados com sucesso!!!");
 	}
 	
 }
