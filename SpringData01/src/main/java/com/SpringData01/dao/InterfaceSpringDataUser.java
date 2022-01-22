1cac0a33726cb91580b0134ad52bad0009d6317b
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-22 20:45:13 -0300

    Atualizado em 22/01/22 - 20h45.

--- SpringData01/src/main/java/com/SpringData01/dao/InterfaceSpringDataUser.java
+++ SpringData01/src/main/java/com/SpringData01/dao/InterfaceSpringDataUser.java
@@ -1,7 +1,9 @@
 package com.SpringData01.dao;
 
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 
 import com.SpringData01.model.Usuario;
 
--- SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
+++ SpringData01/src/test/java/com/SpringData01/AppSpringDataTest.java
@@ -2,6 +2,7 @@ package com.SpringData01;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -9,24 +10,29 @@ import com.SpringData01.dao.InterfaceSpringDataUser;
 import com.SpringData01.model.Usuario;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class AppSpringDataTest {
 
 	//interface para realizar os métodos de CRUD do spring.
+	@Autowired
 	private InterfaceSpringDataUser interfaceSpringDataUser;
-	
+
 	@Test
 	public void testeInsert() {
+
 		Usuario usuario = new Usuario();
+
 		usuario.setEmail("ricardo@gmail.com");
-		usuario.setIdade(51);
+		usuario.setIdade(31);
 		usuario.setLogin("ricardo");
+		usuario.setSenha("r123");
 		usuario.setNome("Ricardo Lima Amaral");
-		usuario.setSenha("r1234");
-		
+
 		interfaceSpringDataUser.save(usuario);
-		
-		System.out.println("Cadastrados: " + interfaceSpringDataUser.count());
+
+		System.out.println("Usuarios cadastrados -> " + interfaceSpringDataUser.count());
+
 	}
+
 	
 }
