6aa34c16b1fd0f154235c5c02f6c33b0142920b7
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-23 23:36:10 -0300

    Telefone 23/01/22 - 23h35.

--- SpringData01/src/main/java/com/SpringData01/model/Telefone.java
+++ SpringData01/src/main/java/com/SpringData01/model/Telefone.java
@@ -2,10 +2,12 @@ package com.SpringData01.model;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Telefone {
@@ -14,7 +16,17 @@ public class Telefone {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String tipo;
+	@Column(nullable = false)
 	private String numero;
+	@ManyToOne(optional = false)
+	private Usuario usuario;
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
 	
 	public Long getId() {
 		return id;
